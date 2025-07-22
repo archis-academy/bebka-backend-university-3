3654e61cb957f9c244272d3277bb96033ab0ea97
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-07-22 23:55:28 +0300

    [BE-2] - instructor class

---
+++ bebka-university-3/src/main/java/com/archisacademy/model/Instructor.java
@@ -0,0 +1,69 @@
+package com.archisacademy.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "instructors")
+public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String instructorName;
+    private long instructorNumber;
+    private String email;
+    private String password;
+
+    //private List<Course> taughtCourses;
+
+    public Instructor() {}
+
+    public Instructor(String instructorName, long instructorNumber, String email, String password) {
+        this.instructorName = instructorName;
+        this.instructorNumber = instructorNumber;
+        this.email = email;
+        this.password = password;
+        //this.taughtCourses = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public long getInstructorNumber() {
+        return instructorNumber;
+    }
+
+    public void setInstructorNumber(long instructorNumber) {
+        this.instructorNumber = instructorNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
