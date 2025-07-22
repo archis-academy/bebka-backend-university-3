4f792852b05b5f205cc292b22edecf311a67663d
Bus-sen <snbusra66@gmail.com>
2025-07-23 00:22:20 +0300

    [BE-3] - create course class

---
+++ bebka-university-3/src/main/java/com/archisacademy/model/Course.java
@@ -0,0 +1,66 @@
+package com.archisacademy.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "course_name", nullable = false)
+    private String courseName;
+    @Column(name = "course_number", unique = true, nullable = false)
+    private long courseNumber;
+
+    //private Instructor courseInstructor;
+    //private List<Student> enrolledStudents;
+
+    public Course() {}
+
+    public Course(String courseName, long courseNumber){
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public long getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(long courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+/*
+    public Instructor getCourseInstructor() {
+        return courseInstructor;
+    }
+
+    public void setCourseInstructor(Instructor courseInstructor) {
+        this.courseInstructor = courseInstructor;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(List<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+ */
+}
---
+++ bebka-university-3/src/main/java/com/archisacademy/service/CourseService.java
@@ -0,0 +1,4 @@
+package com.archisacademy.service;
+
+public class CourseService {
+}
