cbab1ca3b0dc2efdef97047bf459a0a914bd56c0
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-07-25 18:21:17 +0300

    [BE-29] Eğitmen Metodu Oluşturma

--- bebka-university-3/src/main/java/com/archisacademy/dao/InstructorDao.java
+++ bebka-university-3/src/main/java/com/archisacademy/dao/InstructorDao.java
@@ -1,4 +1,10 @@
+
 package com.archisacademy.dao;
 
+import com.archisacademy.model.Course;
+import java.util.List;
+
 public interface InstructorDao {
+
+    void addInstructor(long instructorNumber, String instructorName, String email, List<Course> taughtCourses);
 }
