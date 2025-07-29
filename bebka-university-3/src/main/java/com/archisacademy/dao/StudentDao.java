73c46e4264b3e52daa48d47005e5044e1aeecb2a
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-07-29 16:00:30 +0300

    feat: Öğrenci ekleme metodu DAO katmanına eklendi

--- bebka-university-3/src/main/java/com/archisacademy/dao/StudentDao.java
+++ bebka-university-3/src/main/java/com/archisacademy/dao/StudentDao.java
@@ -1,5 +1,33 @@
-package com.archisacademy.dao;
+package com.archisacademy.dao.impl;
 
-public interface StudentDao {
+import com.archisacademy.dao.StudentDao;
+import com.archisacademy.model.Student;
+import com.archisacademy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+
+public class StudentDaoImpl implements StudentDao {
+
+
+    @Override
+    public void addNewStudent(Student student) {
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.persist(student);
+
+            transaction.commit();
+            System.out.println("Yeni öğrenci başarıyla eklendi: " + student.getFullName());
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.err.println("Öğrenci eklenirken bir hata oluştu!");
+            e.printStackTrace(); // Hatanın tüm teknik detayını konsola yazdırır
+        }
+    }
 }
